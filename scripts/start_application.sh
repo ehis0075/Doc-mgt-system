b0e1c36df40b57bf1b83874fae6f68df59412c69
ehis0075 <ehisjude420@gmail.com>
2023-08-20 14:08:56 +0100

    added a script file

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar /home/ubuntu/Doc-mgt-system/target/*.jar > /home/ubuntu/dms-service.log 2>&1 &
+nohup java -jar /home/ubuntu/target/*.jar > /home/ubuntu/dms-service.log 2>&1 &
